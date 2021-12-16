bebc2ac0369a6a378b22d4d23361a71251fdd79d
Artem Furman <af@Artems-MBP.fios-router.home>
2021-12-15 21:01:23 -0500

    chatbot

---
+++ src/chatBot/data.js
@@ -0,0 +1,36 @@
+export const data = [
+  {
+    question: 'hi',
+    answer: 'Hello',
+  },
+  {
+    question: 'hello',
+    answer: 'Hi',
+  },
+  {
+    question: 'need',
+    answer: 'How many towels do you need?',
+  },
+  {
+    question: 'towels',
+    answer: 'How many towels do you need?',
+  },
+  {
+    question: 'I',
+    answer: 'How many towels do you need?',
+  },
+  {
+    question: `${1-20}`,
+    answer: 'will you need bath towels, hand towels, or washcloths?',
+  },
+
+  {
+    question: 'bath towels, hand towels, washcloths',
+    answer: 'What room number will that be?',
+  },
+  {
+    question: `room`,
+    answer: 'Great!  I will send those up as soon as possible.',
+  },
+
+];
